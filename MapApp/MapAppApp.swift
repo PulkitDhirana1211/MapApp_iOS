2f4de3ba4511bcc67bc0fd979d0dde982f73eac6
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2024-01-01 18:14:41 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Pulkit Dhirana on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
